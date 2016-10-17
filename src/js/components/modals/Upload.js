d403fdbaa90078a5ed6b91e33af9161d9caeb069
Lucas Cotner <lc07@uw.edu>
2016-10-17 15:39:11 -0400

    Added js-shp utils and web workers

--- src/js/components/modals/Upload.js
+++ src/js/components/modals/Upload.js
@@ -6,8 +6,90 @@ import {uploadConfig} from 'js/config';
 import React, { Component } from 'react';
 import appStore from 'js/appStore';
 import esriRequest from 'esri/request';
+import FeatureLayer from 'esri/layers/FeatureLayer';
+import Point from 'esri/geometry/Point';
 // Type Import
 import type {ModalProps} from './Types';
+import shp from 'js/libs/shp';
+import cw from 'catiline';
+
+
+const worker = cw({
+    init: function(scope) {
+      debugger
+        // importScripts('jam/require.js');
+        // require.config({
+        //     baseUrl: this.base
+        // });
+        // require(['shp'], function(shp) {
+        scope.shp = shp;
+        console.log(scope);
+        // });
+    },
+    data: function(data, cb, scope) {
+      console.log(data);
+        this.shp(data).then(function(geoJson){
+          console.log(geoJson);
+            if (Array.isArray(geoJson)){
+                geoJson.forEach(function(geo){
+                    scope.json([geo, geo.fileName, true], true, scope);
+                });
+            } else {
+                scope.json([geoJson, geoJson.fileName, true], true, scope);
+            }
+        }, function(e) {
+            console.log('shit', e);
+        });
+
+    },
+    color: function(s){
+      //from http://stackoverflow.com/a/15710692
+      importScripts('js/colorbrewer.js');
+      return colorbrewer.Spectral[11][Math.abs(JSON.stringify(s).split("").reduce(function(a,b){a=((a<<5)-a)+b.charCodeAt(0);return a&a},0)) % 11];
+    },
+    makeString: function(buffer) {
+      var array = new Uint8Array(buffer);
+      var len = array.length;
+      var outString = '';
+      var i = 0;
+      while (i < len) {
+          outString += String.fromCharCode(array[i++]);
+      }
+      return outString;
+    },
+    json: function(data, cb, scope) {
+      console.log('worker json');
+      // importScripts('js/topojson.v1.min.js');
+      var name = data[1];
+      //console.log(name);
+      var json = data.length === 2 ? JSON.parse(scope.makeString(data[0])) : data[0];
+      var nom;
+      if (json.type === 'Topology') {
+          for (nom in json.objects) {
+              scope.layer(topojson.feature(json, json.objects[nom]), nom, scope);
+          }
+      } else {
+          scope.layer(json, name, scope);
+      }
+    }, layer: function(json, name, scope){
+
+      json.features.forEach(function(feature){
+          feature.properties.__color__ = scope.color(feature);
+      });
+      scope.fire('json', [json, name]);
+    },
+    base: cw.makeUrl('.')
+});
+
+worker.on('json', function(e) {
+    console.log(e);
+    debugger
+    // lc.addOverlay(L.geoJson(e[0], options).addTo(m), e[1]);
+});
+
+worker.on('error', function(e) {
+    console.warn(e);
+});
 
 const TYPE = {
   ZIP: 'application/zip',
@@ -52,7 +134,6 @@ export default class UploadModal extends Component {
   };
 
   drop = (evt) => {
-    console.log('drop');
     this.prevent(evt);
 
     const file = evt.dataTransfer &&
@@ -68,23 +149,172 @@ export default class UploadModal extends Component {
       dndActive: false,
       isUploading: true
     });
-    const type = TYPE.SHAPEFILE;
-    console.log(this.props.map);
-    const params = uploadConfig.shapefileParams(file.name, this.props.map.spatialReference);
-    const content = uploadConfig.shapefileContent(JSON.stringify(params), type);
+    // const type = TYPE.SHAPEFILE;
+    // console.log(this.props.map);
+
+    // const params = uploadConfig.shapefileParams(file.name, this.props.map.spatialReference);
+    // const content = uploadConfig.shapefileContent(JSON.stringify(params), type);
     const input = this.refs.fileInput;
     input.files = evt.dataTransfer.files;
-    console.log(input.files);
+    // console.log(input);
+    // const path = input.files[0]; //.name;
+    // console.log(path);
+
+
+    const inputFile = input.files[0];
+
+    if (inputFile.name.slice(-3) === 'zip') {
+        this.handleZipFile(inputFile);
+    } else {
+      var reader = new FileReader();
+      reader.onload = function() {
+          let ext;
+          if (reader.readyState !== 2 || reader.error) {
+              return;
+          } else {
+            // debugger
+              ext = inputFile.name.split('.');
+              ext = ext[ext.length - 1];
+
+              console.log(worker.json);
+              worker.json([reader.result, inputFile.name.slice(0, (0 - (ext.length + 1)))], [reader.result]);
+          }
+      };
+      reader.readAsArrayBuffer(inputFile);
+    }
+
+    // shp(path).then(function(geojson){
+    //   console.log(geojson);
+    //     //do something with your geojson
+    // });
+
+    // this.getData(path)
+    //   .then(this.createGraphics) // then send it to the createGraphics() method
+    //   .then(this.createLayer) // when graphics are created, create the layer
+    //   .otherwise(this.errback);
   };
 
-  request = () => {
-    esriRequest({
-     url: url,
-     form: form,
-     content: content,
-     handleAs: 'json'
+  handleZipFile = (file) => {
+    const reader = new FileReader();
+    console.log(file);
+    console.log(reader);
+    // debugger
+    // reader.onload = this.readerLoad;
+    reader.onload = function () {
+        const data = reader.result;
+        console.log(data);
+        // const array = new Int8Array(data);
+        // output.value = JSON.stringify(array, null, '  ');
+        // window.setTimeout(ReadFile, 1000);
+    };
+    // reader.onload = () => {
+    //   var data = reader.result;
+    //   console.log('data', data);
+    //   // var array = new Int8Array(data);
+    //   // console.log('array', array);
+    //   // const value = JSON.stringify(array, null, '  ');
+    //   // console.log('value', value);
+    //   // this.readerLoad(data);
+    //   worker.data(data, [data]);
+    // };
+
+    reader.readAsArrayBuffer(file);
+    //I think I need reader.result!
+  }
+
+  readerLoad = (data) => {
+    console.log(data);
+    console.log(arguments);
+
+    // shp(data).then(function(geoJson){
+    //   console.log(geoJson);
+    //   debugger
+      // if(Array.isArray(geoJson)){
+      //     geoJson.forEach(function(geo){
+      //         scope.json([geo, geo.fileName, true],true,scope);
+      //     });
+      // } else {
+      //     scope.json([geoJson, geoJson.fileName, true],true,scope);
+      // }
+    // });
+
+    // if (this.readyState !== 2 || this.error) {
+    //     return;
+    // } else {
+    // worker.json([reader.result, file.name.slice(0, (0 - (ext.length + 1)))], [reader.result]);
+    worker.data(this.result, [this.result]);
+    // }
+  }
+
+  getData = (url) => {
+  //   esriRequest({
+  //    url: url,
+  //   //  form: form,
+  //   //  content: content,
+  //    handleAs: 'json'
+  //  });
+   return esriRequest(url, {
+     responseType: 'json'
    });
   }
+
+  createGraphics = (response) => {
+    // raw GeoJSON data
+    const geoJson = response.data;
+
+    // Create an array of Graphics from each GeoJSON feature
+    return geoJson.forEach(geoJson.features, (feature, i) => {
+      return {
+        geometry: new Point({
+          x: feature.geometry.coordinates[0],
+          y: feature.geometry.coordinates[1]
+        }),
+        // select only the attributes you care about
+        attributes: {
+          ObjectID: i,
+          title: feature.properties.title,
+          // type: feature.properties.type,
+          // place: feature.properties.place,
+          // depth: feature.geometry.coordinates[2] + " km",
+          // time: feature.properties.time,
+          // mag: feature.properties.mag,
+          // mmi: feature.properties.mmi,
+          // felt: feature.properties.felt,
+          // sig: feature.properties.sig,
+          url: feature.properties.url
+        }
+      };
+    });
+  }
+
+  errback = (error) => {
+    console.error('Creating legend failed. ', error);
+  }
+
+  createLayer = (graphics) => {
+    const fields = [];
+
+    graphics[0].attributes.forEach(attribute => {
+      console.log(attribute);
+      fields.push(attribute);
+    });
+
+    const newLayer = new FeatureLayer({
+      source: graphics, // autocast as an array of esri/Graphic
+      // create an instance of esri/layers/support/Field for each field object
+      fields: fields, // This is required when creating a layer from Graphics
+      objectIdField: 'ObjectID', // This must be defined when creating a layer from Graphics
+      // renderer: quakesRenderer, // set the visualization on the layer
+      spatialReference: {
+        wkid: 4326
+      },
+      geometryType: 'point'//, // Must be set when creating a layer from Graphics
+      // popupTemplate: pTemplate
+    });
+
+        this.props.map.add(newLayer);
+        // return lyr;
+      }
 
   // selectTopic:Function = (evt) => {
   //   this.close();
